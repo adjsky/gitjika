f26e7baf33acec9145190950f0196f044cc87f13
adjsky <igorlfmartins@mail.ru>
2025-01-03 18:26:59 +0300

    use nix flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "gitjika";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      utils,
+      ...
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              go
+              golangci-lint
+              just
+            ];
+          };
+      }
+    );
+}
